2357f58dc2de8900c8b455a33b90213eeccc74b5
Akila_Ayanthi <aayanthi@gmail.com>
2022-03-03 15:08:34 +0530

    patch size reduction

--- adversarial_attack/adversarial_attack2.py
+++ adversarial_attack/adversarial_attack2.py
@@ -939,9 +939,9 @@ def visualize(names, model, device, image_path, numpy_patch_path, offset, height
             # Calculate the new bounding box
             bounding_box = predictions[0][0:4].detach().tolist()[0]
 
-            transform = T.ToPILImage()
+            transform = T.ToPILImage(mode='RGB')
             # combined_img = np.transpose(combined_img[0,:,:,:], (1, 2, 0, 3))
-            img = transform(combined_img[0,:,:,:].int())
+            img = transform(combined_img[0,:,:,:])
 
             if save_plots:
             # Construct the figure directory within the directory where the patch is
