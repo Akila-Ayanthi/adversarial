a37d70b7a0d1caa945590542cc78b84a4f549608
Akila_Ayanthi <aayanthi@gmail.com>
2022-03-04 10:43:59 +0530

    reducing the patch size

--- adversarial_attack/adversarial_attack2.py
+++ adversarial_attack/adversarial_attack2.py
@@ -963,8 +963,8 @@ def visualize(names, model, device, image_path, numpy_patch_path, offset, height
             # combined_img = np.transpose(combined_img[0,:,:,:], (1, 2, 0))
             # print(combined_img)
             # img = transform(combined_img[0,:,:,:])
-            # img = T.ToPILImage(mode='L')(combined_img[0])
-            print(combined_img[0][0])
+            img = T.ToPILImage(mode='L')(combined_img[0][0])
+            # print(combined_img[0][0])
 
             # if save_plots:
             # # Construct the figure directory within the directory where the patch is
