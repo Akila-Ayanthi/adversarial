937fc16163e25a56c7f4c576cc86009872988b1d
Akila_Ayanthi <aayanthi@gmail.com>
2022-03-04 15:08:33 +0530

    reducing the patch size

--- adversarial_attack/adversarial_attack2.py
+++ adversarial_attack/adversarial_attack2.py
@@ -969,9 +969,9 @@ def visualize(names, model, device, image_path, numpy_patch_path, offset, height
 
             # img  = combined_img[0,:,:,:].int().permute(1, 2, 0).cpu().numpy() # make sure tensor is on cpu
             # print(img)
-            # c_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-            img = single_image.cpu().numpy()
-            c_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            c_img = cv2.cvtColor(single_image, cv2.COLOR_RGB2BGR)
+            # img = single_image.cpu().numpy()
+            # c_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # print(img)
             # cv2.imwrite(combined_img, "image.png")
             # print(combined_img[0][0])
