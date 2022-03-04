bf05de54a74ed80b6ee8a405029ed11485d7dfc3
Akila_Ayanthi <aayanthi@gmail.com>
2022-03-04 11:57:59 +0530

    reducing the patch size

--- adversarial_attack/adversarial_attack2.py
+++ adversarial_attack/adversarial_attack2.py
@@ -968,7 +968,7 @@ def visualize(names, model, device, image_path, numpy_patch_path, offset, height
             # img = T.ToPILImage(mode=None)(img)
 
             img  = combined_img[0,:,:,:].int().permute(1, 2, 0).cpu().numpy() # make sure tensor is on cpu
-            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            c_img = cv2.cvtColor(img[0], cv2.COLOR_RGB2BGR)
             # img = single_image.cpu().numpy()
             # print(img)
             # cv2.imwrite(combined_img, "image.png")
@@ -985,7 +985,7 @@ def visualize(names, model, device, image_path, numpy_patch_path, offset, height
                 output_name = output_name[:index] + "_adversarial_result.png"
 
             # img = img.save(os.path.join(fig_dir, output_name))
-            cv2.imwrite(str(os.path.join(fig_dir, output_name)), img)
+            cv2.imwrite(str(os.path.join(fig_dir, output_name)), c_img)
 
 
             # # Perform plotting using Pyplot from Matplotlib
